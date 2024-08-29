e457be6f3f919da67f2b3cf6daeaa0252336d991
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-08-29 11:08:03 +0200

    Initial Commit

---
+++ fframes_demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fframes_demo
+//
+//  Created by Dmitriy Kovalenko on 29.08.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fframes_demo/fframes_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  fframes_demoApp.swift
+//  fframes_demo
+//
+//  Created by Dmitriy Kovalenko on 29.08.24.
+//
+
+import SwiftUI
+
+@main
+struct fframes_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
