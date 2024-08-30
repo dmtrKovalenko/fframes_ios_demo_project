ee32b8cc3981a0762d27e449c5307b30c8940e6e
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2024-08-30 12:12:49 +0200

    Init commit

---
+++ BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  fframes_demo
+//
+//  Created by Dmitriy Kovalenko on 30.08.24.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "hello_world.h"
+
+#endif /* BridgingHeader_h */
---
+++ fframes_demo/Demo.swift
@@ -0,0 +1,16 @@
+//
+//  Demo.swift
+//  fframes_demo
+//
+//  Created by Dmitriy Kovalenko on 29.08.24.
+//
+
+import Foundation
+
+class RustGreetings {
+    func sayHello() -> String {
+        fframes_render("ios", "lol")
+    
+        return "yes!"
+    }
+}
